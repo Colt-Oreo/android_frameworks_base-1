8ea9c2879a5134dcd3ee2677c747e5857caa0bcd
maxwen <max.weninger@gmail.com>
2017-08-29 18:25:35 +0200

    Double tap to sleep on statusbar [1/2]
    abc ezio84: use existing lockscreen doubletap gesture listener
    and abcutils screenoff action
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5403,6 +5403,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.AMBIENT_DOZE_CUSTOM_BRIGHTNESS),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -5431,6 +5434,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.AMBIENT_DOZE_CUSTOM_BRIGHTNESS))) {
                 updateDozeBrightness();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE))) {
+                setStatusBarWindowViewOptions();
             }
         }
 
