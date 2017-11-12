87d9dc5c7ebd6d1ae8617c681c286fc5cd6084ec
ezio84 <brabus84@gmail.com>
2017-11-13 00:07:12 +0100

    Comment out dead aosp lowbattery sound code
    Notification.Bulder.setSound is now deprecated and NotificationChannels.ALERTS
    sound will always override it, so attachLowBatterySound() is just dead
    code.
    To replicate: just wait for low battery (5%) and you'll get the default
    Alerts channel sound (that anyway is customizable into SystemUI
    notifications settings) instead of the lowbattery one.
    
    An alternative is to create a new notification channel for the
    lowbattery notification, but i'm more lazy than lazy Google :V
    Let's see what they'll eventually do with this in the future.
    
    Change-Id: I78c421ef5ef16179f582f350d0a07349e7a8d286
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -1398,7 +1398,7 @@ public interface WindowManager extends ViewManager {
          * Window flag: Overrides default power key behavior
          * @hide
          */
-        public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
+//        public static final int PRIVATE_FLAG_PREVENT_POWER_KEY = 0x20000000;
 
         /**
          * Control flags that are private to the platform.
